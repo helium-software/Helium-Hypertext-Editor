a7801fcb48059b74517b3be0426154d924706d43
Tibor Stolz <tstolz@ee.ethz.ch>
2012-09-13 21:25:19 +0200

    tagsheet: interp creation and basic parser procs working. Will be restructured soon. Erzeugen von Interpretern und grundlegende Parser-Funktion läuft. Wird bald restrukturiert.

---
+++ tagsheet/tests/test-interpreter.tcl
@@ -0,0 +1,38 @@
+#!/usr/bin/tclsh8.5
+
+## Make tclIndex accessible
+
+# Relative path to tclIndex - needs to be updated (add/remove some ..)
+#  whenever this script is moved into a different hierarchy level
+set dir [file join [file dirname [info script]] .. ..]
+source [file join $dir tclIndex]
+
+##
+puts "\n   Test if the interpreters are initialized correctly:\n"
+
+::tagsheet::init
+tagsheet::outer-interp eval {puts "Hello from outer-interp"}
+tagsheet::inner-interp eval {puts "Hello from inner-interp"}
+
+##
+puts "\n   Test the inner interpreter:\n"
+
+proc feedInterp {script {interp ::tagsheet::inner-interp}} {
+	if {[string index $script 0]=="\n"}   {set script [string range $script 1 end]}
+	if {[string index $script end]=="\n"} {set script [string range $script 0 end-1]}
+	puts -nonewline ">>: "
+	puts [join [split $script \n] "\n>>: "]
+
+	if [catch [list $interp eval $script] result] {set c E} else {set c o}
+
+	if {$result==""} return
+	puts -nonewline "$c: "
+	puts [join [split $result \n] "\n$c: "]
+}
+feedInterp {Hello Interpreter; say something}
+feedInterp {singleword}
+feedInterp {three toggle words}
+feedInterp {bold toggle}
+feedInterp {size ++ ; margin --;}
+feedInterp {font = "Some cool font"}
+feedInterp {size += 45}
