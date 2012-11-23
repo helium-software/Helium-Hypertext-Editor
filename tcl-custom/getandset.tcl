a4149e9bc64899ff05e90e1ef446a918797b1fa2
Tibor Stolz <tstolz@ee.ethz.ch>
2012-11-23 15:26:34 +0100

    tcl-custom: implemented better "get" procedure via interp alias Bessere (effizientere) get-Prozedur über Interpreter-Alias implementiert.

--- tcl-custom/getandset.tcl
+++ tcl-custom/getandset.tcl
@@ -1,4 +1,7 @@
-proc get {arg} {uplevel [list set $arg]}
+# define "get" as an alias for "set"
+proc get {} {} ;# this is only for the tclIndex
+interp alias "" get "" set
+
 proc set_ifnotexists {variable value} {
 	upvar $variable Variable
 	if {![info exists Variable]} {set Variable $value}
