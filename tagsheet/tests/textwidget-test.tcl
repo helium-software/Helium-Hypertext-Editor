ed121fcce8acef5de352d962317fe40f2a64c369
Tibor Stolz <tstolz@ee.ethz.ch>
2013-01-11 10:31:22 +0100

    tagsheet/textwidget test: implemented Tag Inspection

--- tagsheet/tests/textwidget-test.tcl
+++ tagsheet/tests/textwidget-test.tcl
@@ -17,11 +17,11 @@ namespace import ::tcl::mathop::*
 pack [ttk::panedwindow .pw] -fill both -expand true -padx 1 -pady 1
 .pw add [ttk_text .tagsheet -height 15]
 ttk_text_scrollbar .tagsheet on
-.pw add [ttk::frame .frame]
-pack [ttk_text .rendering -height 15] -in .frame -fill both -expand true 
+.pw add [ttk::notebook .notebook]
+.notebook add [ttk_text .rendering -height 15] -text "Rendering"
 ttk_text_scrollbar .rendering on
 ttk::button .renderbutton -text "Update (Ctrl-R)" -takefocus 0
-pack .renderbutton -in .frame -side top -anchor e -before .rendering_frame -pady {0 2}
+place .renderbutton -in .notebook -anchor ne -y 1 -relx 1
 ttk::style configure TButton -padding 0
 
 bind . <Control-KeyPress-r> {.renderbutton state pressed}
@@ -122,4 +122,62 @@ proc insert_text {} {
 insert_text
 
 ## Update button
-.renderbutton configure -command {reload_tagsheet; configure_tags; insert_text}
+.renderbutton configure -command {reload_tagsheet; configure_tags; insert_text; update_inspector}
+
+### Tag inspection feature ###
+
+## Interface elements
+.notebook add [ttk::frame .inspector] -text "Tag Inspection" -padding 2
+grid rowconfigure .inspector 1 -weight 1
+grid columnconfigure .inspector 0 -weight 1
+grid [ttk::combobox .inspector.cbox -textvariable inspector_cbox] -sticky w -pady 2
+grid [ttk::treeview .inspector.table -height 2] -sticky nesw -pady 2
+place [ttk::scrollbar .inspector.table.scroll -orient vertical -command {.inspector.table yview}] \
+	-anchor se -relx 1 -rely 1 -x -2 -y -3 -relheight 1 -height -31
+.inspector.table configure -columns {option value} -show headings -yscroll {.inspector.table.scroll set}
+.inspector.table heading 0 -text Option
+.inspector.table heading 1 -text Value
+.inspector.table column 0 -width 120
+
+## Table display procedures
+proc display_clear {} { .inspector.table delete [.inspector.table children ""] }
+
+# Extracts the relevant parts (option-name and value) from a [$widget configure] output
+# and displays them in .inspector.table
+proc display_config {data} {
+	display_clear
+	foreach line $data {
+		lassign $line option . . . value
+		if {$value==""} continue
+		.inspector.table insert {} end -values [list $option $value]
+	}
+}
+proc display_message {message} {
+	display_clear
+	.inspector.table insert {} end -values [list $message ""]
+}
+display_message "(Select a tag)"
+
+## Selecting a particular tag
+proc select_tag {tag} {
+	if {$tag == "<text widget>"} {
+		display_config [.rendering configure]
+	} elseif {$tag in [.rendering tag names]} {
+		display_config [.rendering tag configure $tag]
+	} elseif {$tag == ""} {
+		display_message "(Select a tag)"
+	} else {
+		display_message "(Tag does not exist)"
+	}
+}
+bind .inspector.cbox <<ComboboxSelected>> {select_tag $inspector_cbox}
+bind .inspector.cbox <Return> {select_tag $inspector_cbox}
+
+## Updates on re-evaluating the tagsheet
+proc update_inspector {} {
+	global inspector_cbox
+	select_tag $inspector_cbox
+}
+# don't need to update tag list, it is constant
+.inspector.cbox configure -values [concat {"<text widget>"} [.rendering tag names]]
+set inspector_cbox ""
